32974a136b626b2ea2acea7e0537e2ba12106ae7
ASUS <ASUS@DESKTOP-SV986PK>
2023-08-17 18:22:26 +0300

    Add project files.

---
+++ Pokemon/Controllers/IndexController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pokemon.Controllers
+{
+    public class IndexController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult SingleCategory()
+        {
+            return View();
+        }
+    }
+}
